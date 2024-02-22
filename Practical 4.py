8c346a5018929feed0c3daff1d81a4540ec41f0a
Aditya patil <156800442+AdityapatilSP@users.noreply.github.com>
2024-02-22 09:19:03 +0530

    Practical 4.py

--- Practical 4.py
+++ Practical 4.py
@@ -4,29 +4,17 @@ Chemistry = int(input("Enter Chemistry marks :"))
 Physics = int(input("Enter Physics marks :"))
 BEE = int(input("Enter BEE marks :"))
 
-if English > 40:
-    print("Passed in English ")
-else : print("Fail")
-if Maths > 40:
-    print("Passed in Maths ")
-else : print("Fail")
-if Chemistry > 40:
-    print("Passed in Chemistry ")
-else: print("Fail")
-if Physics > 40:
-    print("Passed in Physics")
-else: print("Fail")
-if BEE > 40:
-    print("Passed in BEE")
-else : print("Fail")
 
-Aggregate = ((int(English) + int(Maths) + int(Chemistry) + int(Physics) + int(BEE)) / 500)*(100)
-print("The Aggregate is : ", Aggregate)
-
-
-if Aggregate > 75:
-    print("The grade is Distinction ")
-elif Aggregate >= 60:
-    print("The grade is First Division")
-elif Aggregate >= 50:
-    print("The grade is Second Division")
+if (English >= 40 and Maths >=40 and Chemistry >= 40 and Physics >=40 and BEE >=40):
+    print("Passed")
+    Aggregate = ((int(English) + int(Maths) + int(Chemistry) + int(Physics) + int(BEE)) / 500)*(100)
+    print("The Aggregate is : ", Aggregate)
+    if Aggregate > 75:
+        print("The grade is Distinction ")
+    elif Aggregate >= 60:
+        print("The grade is First Division")
+    elif Aggregate >= 50:
+        print("The grade is Second Division")
+    elif Aggregate >= 40:
+        print("The grade is third Division ")
+else : print("Failed")
